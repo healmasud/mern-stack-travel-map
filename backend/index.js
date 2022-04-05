30cb587095627c491540f17b13fd20abb046322c
Md Abdulla Heal Masud <heal.masud@gmail.com>
2022-04-05 09:31:14 +0300

    commit

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const pinRoute = require("./routes/pins");
+const app = express();
+
+dotenv.config();
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Connected");
+  })
+  .catch((err) => console.log(err));
+
+app.use("/api/pins", pinRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
---
+++ backend/routes/pins.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+const Pin = require("../models/Pin");
+
+// create a pin
+router.post("/", async (req, res) => {
+  const newPin = new Pin(req.body);
+  try {
+    const savedPin = await newPin.save();
+    res.status(200).json(savedPin);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
