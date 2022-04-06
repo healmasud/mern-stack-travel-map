b016733eb4043e81b3109b184a958393469b30e4
Md Abdulla Heal Masud <heal.masud@gmail.com>
2022-04-06 09:40:10 +0300

    login and register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,20 @@ import { Room, Star } from "@material-ui/icons";
 import axios from "axios";
 import { format } from "timeago.js";
 import "./app.css";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 function App() {
-  const currentUser = "masud";
+  const myStorage = window.localStorage;
+  const [currentUser, setCurrentUser] = useState(myStorage.getItem("user"));
   const [pins, setPins] = useState([]);
   const [currentPlaceId, setCurrentPlaceId] = useState(null);
   const [newPlace, setNewPlace] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [desc, setDesc] = useState(null);
+  const [showRegister, setShowRegister] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [rating, setRating] = useState(0);
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -44,6 +52,30 @@ function App() {
     });
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentUser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.long,
+    };
+    try {
+      const res = await axios.post("/pins", newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleLogout = () => {
+    myStorage.removeItem("user");
+    setCurrentUser(null);
+  };
+
   return (
     <div className="App">
       <ReactMapGL
@@ -52,15 +84,15 @@ function App() {
         onViewportChange={(nextViewport) => setViewport(nextViewport)}
         mapStyle="mapbox://styles/healmasud/cl1lyr23z002s14qayhdct9wd"
         onDblClick={handleAddClick}
-        transitionDuration="200"
+        // transitionDuration="200"
       >
         {pins.map((p) => (
           <>
             <Marker
               latitude={p.lat}
               longitude={p.long}
-              offsetLeft={-20}
-              offsetTop={-10}
+              offsetLeft={-viewport.zoom * 3.5}
+              offsetTop={-viewport.zoom * 7}
             >
               <Room
                 style={{
@@ -87,11 +119,7 @@ function App() {
                   <p className="desc">{p.desc}</p>
                   <label>Rating</label>
                   <div className="stars">
-                    <Star className="star" />
-                    <Star className="star" />
-                    <Star className="star" />
-                    <Star className="star" />
-                    <Star className="star" />
+                    {Array(p.rating).fill(<Star className="star" />)}
                   </div>
                   <label>Information</label>
                   <span className="username">
@@ -113,18 +141,24 @@ function App() {
             onClose={() => setNewPlace(null)}
           >
             <div>
-              <form>
+              <form onSubmit={handleSubmit}>
                 <label>Title</label>
-                <input placeholder="Enter a title" />
+                <input
+                  placeholder="Enter a title"
+                  onChange={(e) => setTitle(e.target.value)}
+                />
                 <label>Review</label>
-                <textarea placeholder="Say something about this place" />
+                <textarea
+                  placeholder="Say something about this place"
+                  onChange={(e) => setDesc(e.target.value)}
+                />
                 <label>Rating</label>
-                <select>
+                <select onChange={(e) => setRating(e.target.value)}>
                   <option value="1">1</option>
-                  <option value="1">2</option>
-                  <option value="1">3</option>
-                  <option value="1">4</option>
-                  <option value="1">5</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
                 </select>
                 <button className="submitButton" type="submit">
                   Add Pin
@@ -132,6 +166,31 @@ function App() {
               </form>
             </div>
           </Popup>
+        )}
+        {currentUser ? (
+          <button className="button logout" onClick={handleLogout}>
+            Logout
+          </button>
+        ) : (
+          <div className="buttons">
+            <button className="button login" onClick={() => setShowLogin(true)}>
+              Login
+            </button>
+            <button
+              className="button register"
+              onClick={() => setShowRegister(true)}
+            >
+              Register
+            </button>
+          </div>
+        )}
+        {showRegister && <Register setShowRegister={setShowRegister} />}
+        {showLogin && (
+          <Login
+            setShowLogin={setShowLogin}
+            myStorage={myStorage}
+            setCurrentUser={setCurrentUser}
+          />
         )}
       </ReactMapGL>
     </div>
--- readme.md
+++ readme.md
@@ -1,7 +1,49 @@
-# [work in progress]
+# MERN stack travel map
 
-MERN stack travel map application
+## Description
+
+An travel map application where user can register, login and save their favourite places in the map. This application is built with MERN stack. This application features:
+
+- Node provides the backend environment for this application
+- Express middleware is used to handle requests, routes
+- MongoDB provides the database
+- React for displaying UI components
+- MapBox provides the mapping functionalities
+
+## Install Dependencies
+
+**For Backend** - `cd backend` `npm install` `npm start`
+
+**For Frontend** - `cd frontend` `npm install` `npm start`
+
+## Env Variables
+
+Make Sure to Create a .env file and add appropriate variables in order to use the app.
+
+### Essential Variables (frontend)
+
+REACT_APP_MAPBOX =
+
+### Essential Variables (backend)
+
+MONGO_URL =
+
+## Languages & tools
+
+- [Node](https://nodejs.org/en/)
+- [Express](https://expressjs.com/)
+- [MongoDB](https://mongodb.com/)
+- [React](https://reactjs.org/)
+- [Webpack](https://webpack.js.org/)
 
 ## Home
 
-![Pic](https://raw.githubusercontent.com/healmasud/mern-stack-travel-map/master/github-overview/wip.png)
+![Pic](https://raw.githubusercontent.com/healmasud/mern-stack-travel-map/master/github-overview/home.png)
+
+## Home
+
+![Pic](https://raw.githubusercontent.com/healmasud/mern-stack-travel-map/master/github-overview/register.png)
+
+## Home
+
+![Pic](https://raw.githubusercontent.com/healmasud/mern-stack-travel-map/master/github-overview/login.png)
